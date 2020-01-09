b3237ffd6b9d8f452bf27b98e734512de91ca5a5
world-mysteries <dongatshj@gmail.com>
2020-01-09 10:50:22 +0800

    auto commit source

--- publish.sh
+++ publish.sh
@@ -1,11 +1,12 @@
-git pull
 simiki g
 
+git pull
 git add .
 git commit -m "auto commit source"
 git push
 
 cd ./output
+git pull
 git add .
 git commit -m "auto commit master"
 git push -u origin master
